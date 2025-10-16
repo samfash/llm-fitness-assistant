c7de29716a40f828a016dea05d63af293b2b87b4
samuel fasanya <samuelfasanya351@gmail.com>
2025-10-16 03:54:10 +0100

    mistralai model change

--- core/llm_engine.py
+++ core/llm_engine.py
@@ -1,10 +1,12 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 from peft import PeftModel
 
+model_name = "mistralai/Mistral-7B-v0.1"  # or "tiiuae/falcon-7b-instruct" or "meta-llama/Llama-2-7b-chat-hf"
+
 # Load fine-tuned weights
-base = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
+base = AutoModelForCausalLM.from_pretrained( model_name)
 model = PeftModel.from_pretrained(base, "./lora-fitness")
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
 
